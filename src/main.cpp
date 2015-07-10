355cebbdfc6be2d91aa98167d78230957a0aeae5
TennyZhuang <zhuangtianyi@TennydeMacBook-Pro.local>
2015-07-10 16:14:03 +0800

    main

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,24 @@
 #include "rststrategy.h"
 
 int main(int argc, char const *argv[]) {
-    RST rst;
+    RST* rst;
+    // if (!rst->getStrategy())
+    // {
+    //     QMessageBox msgBox;
+    //     msgBox.setText("Error: No Algorithm Selected. ");
+    //     msgBox.exec();
+    //     return 0;
+    // }
+    // if (!rst->hasData())
+    // {
+    //     QMessageBox msgBox;
+    //     msgBox.setText("Error: No Input Data. ");
+    //     msgBox.exec();
+    //     return 0;
+    // }
+
+    rst->changeStrategy(1);
+    rst->toSolveRST();
+    // delete imp;
     return 0;
 }
