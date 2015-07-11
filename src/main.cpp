df8c1425f6bf61a7e7db488b6e6e29aa3dc071f8
TennyZhuang <zhuangtianyi@TennydeMacBook-Pro.local>
2015-07-12 02:10:23 +0800

    cli

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,23 @@
 
 int main(int argc, char const *argv[]) {
     const int n = NUM;
+
     RST* rst = new RST;
-    rst->changeStrategy(2);
     PointsGenerator* p = new PointsGenerator;
     std::string filename = "../test/data_0.txt";
-//    p->print_points_to_file(filename, NUM, MAX_RANGE);
+
+    rst->changeStrategy(ZRST);
+
+    if (argc > 1) {
+        if (!strcmp(argv[1], "ZRST"))
+            rst->changeStrategy(ZRST);
+        else if (!strcmp(argv[1], "LRST"))
+            rst->changeStrategy(LRST);
+        else if (!strcmp(argv[1], "test")){
+            p->print_points_to_file(filename, NUM, MAX_RANGE);
+        }
+    } 
+
     std::vector<Point> points = p->generate_points_from_file(filename);
     rst->loadPoints(points);
     rst->solve();
