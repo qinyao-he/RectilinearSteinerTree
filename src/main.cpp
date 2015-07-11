9d95ac4b9b592b8479288e1cfb22c441ced5f53a
TennyZhuang <zhuangtianyi@TennydeMacBook-Pro.local>
2015-07-11 23:34:46 +0800

    add test

--- src/main.cpp
+++ src/main.cpp
@@ -9,18 +9,25 @@ int main(int argc, char const *argv[]) {
     const int n = NUM;
     RST* rst = new RST;
     rst->changeStrategy(2);
-    std::mt19937 generator;
-    std::uniform_int_distribution<int> uniform(0, SCREEN_SIZE);
-    for (int i = 0; i < n; i++) {
-        int x, y;
-        x = uniform(generator);
-        y = uniform(generator);
-        rst->addPoint(x, y);
-    }
+    PointsGenerator* p = new PointsGenerator;
+    std::string filename = "../test/data_0.txt";
+    p -> print_points_to_file(filename, NUM, MAX_RANGE);
+    std::vector<Point> points = p->generate_points_from_file(filename);
+    rst->loadPoints(points);
+//    std::mt19937 generator;
+//    std::uniform_int_distribution<int> uniform(0, SCREEN_SIZE);
+//    for (int i = 0; i < n; i++) {
+//        int x, y;
+//        x = uniform(generator);
+//        y = uniform(generator);
+//        rst->addPoint(x, y);
+//    }
     rst->solve();
 
     Visualizer* visualizer = Visualizer::instance();
     visualizer -> show(rst);
+
+    delete rst;
     delete visualizer;
     return 0;
 }
