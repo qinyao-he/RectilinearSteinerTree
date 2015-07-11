afd2bf320405a31cfcbab8f1c4f5e2f16ca36eae
TennyZhuang <zhuangtianyi@TennydeMacBook-Pro.local>
2015-07-11 16:04:17 +0800

    rewrite PointGenerator && add common

--- src/PointsGenerator.cpp
+++ src/PointsGenerator.cpp
@@ -1,116 +1,60 @@
 //
-//  PointsGenerator.cpp
-//  RectilinearSteinerTree
+// Created by ZhuangTianYi on 15/7/11.
 //
-//
-//  TODO(Liang_Xihao)
-//
+
+#include "PointsGenerator.h"
 
 #include <ctime>
 #include <cstdlib>
 #include <fstream>
-#include "PointsGenerator.h"
+#include <string>
+#include <set>
+
 #include "RST.h"
+#include "common.h"
 
-
-bool BinaryTree::insertNode(const Point *_point, Node *&_root) {
-    if (_root == NULL) {
-        _root = new Node(_point);
-        return true;
-    }
-    else if (*_root > _point) return insertNode(_point, _root->leftChild);
-    else if (*_root < _point) return insertNode(_point, _root->rightChild);
-    else return false; //which means the point is repeated
-}
-
-void BinaryTree::exportToVector(std::vector<Point> *pointList, Node *_root) {
-    if (_root != NULL) {
-        exportToVector(pointList, _root->leftChild);
-        pointList->push_back(_root->point);
-        exportToVector(pointList, _root->rightChild);
-    }
-}
-
-void BinaryTree::clear(Node *_root) {
-    if (_root != NULL) {
-        clear(_root->leftChild);
-        clear(_root->rightChild);
-        delete _root;
-    }
-}
-
-BinaryTree::~BinaryTree() {
-    clear(Root);
-}
-
-std::vector<Point> PointsGenerator::byRandom(int num, int maxRange) {
-    srand(clock());
-    int pointsCount = 0;
-    BinaryTree binaryTree;
-    while (pointsCount < num) {
-        int x = rand() % maxRange;
-        int y = rand() % maxRange;
-        Point *tmpPoint = new Point(x, y);
-        if (binaryTree.insertNode(tmpPoint, binaryTree.Root))
-            pointsCount++;
-        delete tmpPoint;
-    }
-
-    std::vector<Point> pointList;
-    pointList.clear();
-    binaryTree.exportToVector(&pointList, binaryTree.Root);
-
-    return pointList;
-}
-
-void PointsGenerator::printPointsToFile(const char *filename, int num, int maxRange) {
-    using std::vector;
-    vector<Point> points = byRandom(num, maxRange);
+void PointsGenerator::printPointsToFile(const std::string& filename, int num = NUM, int maxRange = MAX_RANGE) {
+    std::set<Point> points;
+    std::srand(std::time(nullptr));
     std::ofstream fout(filename);
-    for (vector<Point>::iterator i = points.begin(); i != points.end(); ++i) {
+
+    while (points.size() < num) {
+        int x = std::rand() % maxRange;
+        int y = std::rand() % maxRange;
+        points.insert(Point(y));
+    }
+    for (std::set<Point>::iterator i = points.begin(); i != points.end(); ++i) {
         i->print(fout);
     }
     fout.close();
 }
 
 std::vector<Point> PointsGenerator::fromFile(const std::string& filename) {
-    BinaryTree binaryTree;
-
-    std::ifstream ifile(filename);
-    while (ifile) {
+    std::ifstream fin(filename);
+    std::vector<Point> points;
+    while (fin) {
         int x, y;
-        ifile >> x;
-        if (ifile >> y) {
-            Point *tmpPoint = new Point(x, y);
-            binaryTree.insertNode(tmpPoint, binaryTree.Root);
-            delete tmpPoint;
+        fin >> x;
+        if (fin >> y) {
+            points.push_back(Point(x, y));
         }
     }
-    ifile.close();
+    fin.close();
 
-    std::vector<Point> pointList;
-    pointList.clear();
-    binaryTree.exportToVector(&pointList, binaryTree.Root);
-
-    return pointList;
+    return points;
 }
 
 std::vector<Point> PointsGenerator::fromRST(RST *rst) {
-    BinaryTree binaryTree;
-
+    std::vector<Point> points;
     for (size_t i = 0; i < rst->v_op.size(); i++) {
-        int x, y;
-        x = rst->v_op[i].x, y = rst->v_op[i].y;
+        int x = rst->v_op[i].x;
+        int y = rst->v_op[i].y;
 
-        Point *tmpPoint = new Point(x, y);
-        binaryTree.insertNode(tmpPoint, binaryTree.Root);
-        delete tmpPoint;
+        points.push_back(Point(x, y));
     }
-    std::vector<Point> pointList;
-    pointList.clear();
-    binaryTree.exportToVector(&pointList, binaryTree.Root);
 
-    return pointList;
+    return points;
 }
+
 
 
